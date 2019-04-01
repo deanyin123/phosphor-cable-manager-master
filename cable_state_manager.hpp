00bdf98fe93c2411e49768821ebcd2e790aa9d9c
deanyin123 <40753792+deanyin123@users.noreply.github.com>
2019-04-01 15:47:49 +0800

    Update cable_state_manager.hpp

--- cable_state_manager.hpp
+++ cable_state_manager.hpp
@@ -1,5 +1,11 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string>
 #include <functional>
 #include <sdbusplus/bus.hpp>
 #include <xyz/openbmc_project/Cable/Cable/server.hpp>
@@ -30,13 +36,13 @@ class Cable : public CableInherit
      * @param[in] busName   - The Dbus name to own
      * @param[in] objPath   - The Dbus object path
      */
-    Cable(sdbusplus::bus::bus& bus, const char* objPath) :
+    Cable(sdbusplus::bus::bus& bus, const char* objPath, const uint32_t cableNum) :
         CableInherit(bus, objPath, true), bus(bus), fp(NULL) {    
 			if(Open() == -1) {
-				std::cerr << "Open error!" <<std::endl;
+				std::cerr << "Open error!" << std::endl;
 		}
 		
-		SlotAddr(ssdaddr);
+		slotAddr(cableNum);
 
 		emit_object_added();
     };
@@ -44,24 +50,24 @@ class Cable : public CableInherit
 	Cable() {
 	};
 
-	~Close() {
+	~Cable() {
 		Close();
 	};
 	
 	int Open();
-	uint32_t GetCableData(const std::string cableName)
+	int32_t GetCableData(const std::string cableName)
 	vector<string> Split(const string& info,const string& pattern);
 	void Close();
 	
-	uint32_t CableType() const override;
-	uint32_t Present() const override;
-	uint32_t LinkSpeed() const override;
-	uint32_t LinkWidth() const override;
-	uint32_t LinkActive() const override;
-	uint32_t PartitionID() const override;
-	uint32_t Invalid() const override;
-	uint32_t UspDsp() const override;
-	uint32_t Status() const override;
+	uint32_t cableType() const override;
+	uint32_t present() const override;
+	uint32_t linkSpeed() const override;
+	uint32_t linkWidth() const override;
+	uint32_t linkActive() const override;
+	uint32_t partitionID() const override;
+	uint32_t invalid() const override;
+	uint32_t uspDsp() const override;
+	uint32_t status() const override;
 	
 
 	
